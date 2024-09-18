f2757c6259f0e197aa326ade53c426350942e9c6
bobbyb97 <r.jonas.bush@gmail.com>
2024-09-18 10:49:58 -0400

    Changed script to process sets of files one by one

--- STEP2.bwa_mem.sh
+++ STEP2.bwa_mem.sh
@@ -12,7 +12,7 @@ input_dir="<your_input_directory>"
 REF="<your_reference_genome>"
 
 # Index the reference genome (only needs to be done once)
-bwa index ${REF}
+# bwa index ${REF}
 
 ##--NOTHING BELOW THIS LINE SHOULD BE MODIFIED--##
 
@@ -24,7 +24,7 @@ align_reads() {
     local output="$3"
     # Align the files to genome
     echo "Processing $r1 $r2 $output"
-    bwa mem -T 40 ${REF} "$r1" "$r2" | samtools view -b | samtools sort > "${output}.bam"
+    # bwa mem -T 40 ${REF} "$r1" "$r2" | samtools view -b | samtools sort > "${output}.bam"
 }
 # Generate the array of file pairs
 file_pairs=()
@@ -38,11 +38,19 @@ for r1 in "$input_dir"/*_R1.fq.gz; do
     fi
 done
 
-# Export variables to the parallel environment
-export -f align_reads
-export REF
-export input_dir
+for x in "${file_pairs[@]}"; do
+align_reads $x;
+done
 
-# Use GNU Parallel to run the align_reads function in parallel on the paired reads in the file_pairs array
 
-parallel --colsep ' ' -j $SLURM_NTASKS align_reads ::: "${file_pairs[@]}"
+## The following code is designed to align reads in parallel
+## bwa-mem struggles with memory issues with this approach
+
+## Export variables to the parallel environment
+# export -f align_reads
+# export REF
+# export input_dir
+
+## Use GNU Parallel to run the align_reads function in parallel on the paired reads in the file_pairs array
+
+# parallel --colsep ' ' -j $SLURM_NTASKS align_reads ::: "${file_pairs[@]}"
