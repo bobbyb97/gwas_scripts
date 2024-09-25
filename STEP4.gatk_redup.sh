c4e9823e875188f4ade99bd0f46b59b5da825738
bobbyb97 <r.jonas.bush@gmail.com>
2024-09-25 14:47:27 -0400

    Edits for step4

--- STEP4.gatk_redup.sh
+++ STEP4.gatk_redup.sh
@@ -7,17 +7,19 @@
 #SBATCH --mem-per-cpu 8000
 
 
-input_dir="your_input_dir"
-output_dir="your_output_dir"
+input_dir="trimmed_fastq"
+output_dir="redup_reads/"
 
 file_pairs=()
+
+##--NOTHING BELOW THIS LINE SHOULD BE MODIFIED--##
 
 # Generating array of file names
 # Separating strings for later use
 for r1 in "$input_dir"/*_R1.fq.gz; do
     r2="${r1/_R1.fq.gz/_R2.fq.gz}"
     if [[ -f "$r2" ]]; then
-        output="${r1/_R1.fq.gz/}"
+        output="$(basename "${r1/_R1.fq.gz/}")"
         file_pairs+=("$r1 $r2 $output")
     else
         echo "Warning: No matching R2 file for $r1" >&2
@@ -26,5 +28,6 @@ done
 
 for input in "${file_pairs[@]}"; do
     IFS=' ' read -r r1 r2 output <<< "$input"
-gatk MarkDuplicatesSpark -I ${output_dir}${output}_fixed.bam -M ${output_dir}Metrics/${output}_dedup_metrics.txt -O ${output_dir}BAMS/${output}_sorted_dedup_reads.bam
+    echo ${r1} ${r2} ${output}
+    gatk MarkDuplicatesSpark -I ${input_dir}/${output}_fixed.bam -M ${input_dir}/${output_dir}/${output}_dedup_metrics.txt -O $input_dir/${output_dir}/${output}_sorted_dedup_reads.bam
 done
