aff77a917ac62ab5645f4d347882b3f1f4d8de23
bobbyb97 <r.jonas.bush@gmail.com>
2024-09-25 11:35:47 -0400

    Updating STEP3 and rewriting some variables for reproducibility in STEP2 and STEP7

--- STEP7.index_genome.sh
+++ STEP7.index_genome.sh
@@ -5,6 +5,6 @@
 #SBATCH --mail-type=ALL,TIME_LIMIT_80
 #SBATCH --mail-user=rjb6794
 #Build bwa index for reference genome
-bwa index /storage/home/tpd5366/scratch/NGS/230310_VH00707_75_AAC2C2GHV/fastq/refs/GCF_000188095.3_BIMP_2.2_genomic.fna 2>>bwa.log.txt
+bwa index <REF> >>bwa.log.txt
 #Build IGV index for the reference genome
-samtools faidx /storage/home/tpd5366/scratch/NGS/230310_VH00707_75_AAC2C2GHV/fastq/refs/GCF_000188095.3_BIMP_2.2_genomic.fna
+samtools faidx <REF>
